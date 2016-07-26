c1a4bf6cd8cf3c80a313085aea6eeeae14ec0823
Aman Mavai <amanmavai@gmail.com>
2016-07-26 07:42:43 +0000

    Adding Changes to BinaryHeap

--- Data Structures/BinaryHeap.js
+++ Data Structures/BinaryHeap.js
@@ -1,108 +1,96 @@
 //Set up the namespaces for the application
-var BasicHeap = BasicHeap || {};
-BasicHeap.Simple = BasicHeap.Simple || {};
+var MYAPP = MYAPP || {};
 
 
-BasicHeap.Simple = (function BinaryMinHeap() {
-    var _heapList = [0];
-    var _currentSize = 0;
-
-    function _swap(l, r) {
-        var temp = _heapList[l];
-        _heapList[l] = _heapList[r];
-        _heapList[r] = temp;
-    }
+MYAPP.HeapApi = (function() {
+    var Heap = function() {
+        this.heapList = [0];
+        this.currentSize = 0;
+    };
     
-    function _minChild(i,len) {
+    Heap.prototype.minChild = function(i,len) {
        if((2*i+1) > len){
            return 2*i;
        }else{
-           if(_heapList[2*i]<_heapList[(2*i+1)]){
+           if(this.heapList[2*i]<this.heapList[(2*i+1)]){
                return 2*i;
            }else{
                return (2*i + 1);
            }
        }
-    }
+    };
+
+    Heap.prototype.swap = function(l, r) {
+        var temp = this.heapList[l];
+        this.heapList[l] = this.heapList[r];
+        this.heapList[r] = temp;
+    };
     
-    function _buildHeap(arr) {
+    Heap.prototype.buildHeap = function(arr) {
         var i = Math.floor(arr.length/2);
         
-        arr.unshift(0);_heapList = arr;
+        arr.unshift(0);this.heapList = arr;
         
-        _currentSize = arr.length -1;
+        this.currentSize = arr.length -1;
         while(i>0){
-            _bubbleDown(i,_currentSize);
+            this.bubbleDown(i,this.currentSize);
             i--;
         }
-        return _heapList;
-    }
+        return this.heapList;
+    };
     
-    function _bubbleUp(index) {
+    Heap.prototype.bubbleUp = function(index) {
         while (Math.floor(index / 2) > 0) {
-            if (_heapList[index] < _heapList[Math.floor(index / 2)]) {
-                _swap(index, Math.floor(index / 2));
+            if (this.heapList[index] < this.heapList[Math.floor(index / 2)]) {
+                this.swap(index, Math.floor(index / 2));
             }
             index = Math.floor(index / 2);
         }
-    }
+    };
     
-    function _bubbleDown(index,len) {
+    Heap.prototype.bubbleDown = function(index,len) {
         var mc;
         while (Math.floor(2*index) <= len) {
-            mc = _minChild(index,len);
-            if(_heapList[index]>_heapList[mc]){
-                _swap(index,mc);
+            mc = this.minChild(index,len);
+            if(this.heapList[index]>this.heapList[mc]){
+                this.swap(index,mc);
             }else{
                 break;
             }
             index = mc;
         }
-    }
+    };
     
-    function _insert(key) {
-        _heapList.push(key);
-        _currentSize++;
-        _bubbleUp(_currentSize);
-    }
+    Heap.prototype.insert = function(key) {
+        this.heapList.push(key);
+        this.currentSize++;
+        this.bubbleUp(this.currentSize);
+    };
     
     //Deleting the Root Element(Its the minimum)
-    function _deleteMin() {
-        var res = _heapList[1];
+    Heap.prototype.deleteMin = function() {
+        var res = this.heapList[1];
         
-        _heapList[1] = _heapList[_currentSize]; //Moving the last item to root
-        _currentSize--;
-        _heapList.pop();
+        this.heapList[1] = this.heapList[this.currentSize]; //Moving the last item to root
+        this.currentSize--;
+        this.heapList.pop();
         
-        _bubbleDown(1,_currentSize);
+        this.bubbleDown(1,this.currentSize);
         return res;
-    }
-    function _heapSort(arr) {
-        _buildHeap(arr);
-        var i = _currentSize;
+    };
+    Heap.prototype.heapSort = function(arr) {
+        this.buildHeap(arr);
+        var i = this.currentSize;
         while(i>1){
-             _swap(1,i);
+             this.swap(1,i);
              i--;
-             _bubbleDown(1,i);
+             this.bubbleDown(1,i);
         }
-       return _heapList.slice(1);
-    }
-    
-    function heapList() {
-        return _heapList;
-    }
-     function currentSize() {
-        return _currentSize;
-    }
+       return this.heapList.slice(1);
+    };
+
     var publicApi = {
-        heapList:heapList,
-        currentSize:currentSize,
-        buildHeap:_buildHeap,
-        insert: _insert,
-        deleteMin:_deleteMin,
-        bubbleUp: _bubbleUp,
-        bubbleDown:_bubbleDown,
-        heapSort:_heapSort
+        Heap:Heap
     };
 
     return publicApi;
@@ -111,18 +99,18 @@ BasicHeap.Simple = (function BinaryMinHeap() {
 
 function main(){
     var arr = [9, 11, 6, 5, 2, 8, 3];
-    var res = BasicHeap.Simple.heapSort(arr);
-    console.log(res);
-    console.log(BasicHeap.Simple.heapList());
-    
-    // BasicHeap.Simple.buildHeap(arr);
-    // console.log(arr);
-    // BasicHeap.Simple.insert(1);
-    // console.log(arr);
-    // BasicHeap.Simple.insert(11);
-    // console.log(arr);
-    // BasicHeap.Simple.deleteMin();
-    // console.log(arr);
+    var heap = new MYAPP.HeapApi.Heap();
+    heap.heapSort(arr);
+    console.log(heap);
+
+    // heap.buildHeap(arr);
+    // console.log(heap);
+    // heap.insert(1);
+    // console.log(heap);
+    // heap.insert(13);
+    // console.log(heap);
+    // heap.deleteMin();
+    // console.log(heap);
 }
 
 main();
